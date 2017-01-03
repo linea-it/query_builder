77e2461c0c1edc429f1c6e4b786e09f676185828
lucasdpn <lucasdpnunes@gmail.com>
2017-01-03 09:29:25 -0200

    The idea is to transfer the operations order to inout/data.json file.

--- __init__.py
+++ __init__.py
@@ -2,6 +2,17 @@ from model import queries
 import settings
 
 
+def dfs_pre_order(node):
+    if 'sub_op' in node:
+        dfs_pre_order(node['sub_op'])
+    if 'op' in node:
+        print(node['description'])
+    else:
+        for sub_node in node.keys():
+            dfs_pre_order(node[sub_node])
+    return
+
+
 if __name__ == "__main__":
     exp_time = queries.Operation('exposure_time')
     print(exp_time.query())
--- model/queries.py
+++ model/queries.py
@@ -12,13 +12,14 @@ class Operation():
         self.operation_type = operation_type
 
         self._stm = None
-        self.set_operation(operation_type)
+        self._set_operation(operation_type)
 
-    def set_operation(self, operation_type):
+    def _set_operation(self, operation_type):
         # register operations
         operations = {}
         operations[ExposureTime.OP] = ExposureTime()
         operations[BadRegions.OP] = BadRegions()
+        operations[CombinedMaps.OP] = CombinedMaps()
 
         if operation_type not in operations:
             raise "This operations is not registered"
@@ -40,19 +41,25 @@ class Operation():
             con.execute(op.DropTable(self.save_at()))
 
     def save_at(self):
-        return self.operation_type + "_" + input_settings.PROCESS['id']
+        return self.operation_name() + "_" + input_settings.PROCESS['id']
 
 
 class Statement():
     def get_statement(self):
         raise NotImplementedError("Implement this method")
 
+    def operation_name(self):
+        raise NotImplementedError("Implement this method")
 
-class ExposureTime(Statement):
-    OP = "exposure_time"
+
+class GreatEqual(Statement):
+    OP = "great_equal"
+
+    def __init__(self, param):
+        self.param = param
+        # checks ?
 
     def get_statement(self):
-        element = {'band': 'g', 'value': '0.55', 'name': 'exposure_time_i'}
         table = dal.tables[ExposureTime.OP]
         stm = select(
           [
@@ -60,16 +67,23 @@ class ExposureTime(Statement):
             table.c.signal,
             table.c.ra,
             table.c.dec
-          ]).where(table.c.signal >= literal_column(element['value']))
+          ]).where(table.c.signal >= literal_column(self.param['value']))
         return stm
+
+    def operation_name(self):
+        return self.param('name')
+
 
 
 class BadRegions(Statement):
     OP = 'bad_regions'
 
+    def __init__(self, param):
+        self.param = param
+
     def get_statement(self):
         mask = 0
-        for element in input_settings.OPERATIONS['bad_regions']:
+        for element in self.param:
             mask += int(element['value'])
 
         print ('Mask = %d' % mask)
@@ -84,3 +98,103 @@ class BadRegions(Statement):
           ]).where(op.BitwiseAnd(table.c.signal,
                    literal_column(str(mask))) > literal_column('0'))
         return stm
+
+    def operation_name(self):
+        return BadRegions.OP
+
+
+class SystematicMap(Statement):
+    OP = "systematic_map"
+
+    def __init__(self):
+        self._maps = []
+        self._tables_name = []
+
+        if BadRegions.OP in self._input:
+            bad_regions = Operation(BadRegions.OP)
+            self._maps.append(bad_regions)
+            self._unite.append(bad_regions.save_at())
+
+        if RadialMap.OP in self._input:
+            radial_map = Operation(RadialMap.OP)
+            self._maps.append(radial_map)
+            self._unite.append(radial_map.save_at())
+
+        if CombinedMaps.OP in self._input:
+            combined_maps = Operation(CombinedMaps.OP)
+            self._maps.extend(combined_maps.all_maps())
+            self._intersect.append(combined_maps.tables_to_intersect())
+
+    def get_statement(self):
+        table = dal.tables[ExposureTime.OP]
+        stm = select(
+          [
+            table.c.pixel,
+            table.c.signal,
+            table.c.ra,
+            table.c.dec
+          ]).where(table.c.signal >= literal_column(self.element['value']))
+        return stm
+
+    def operation_name(self):
+        return self.element('name')
+
+
+class CombinedMaps(Statement):
+    OP = 'combined_maps'
+
+    def __init__(self):
+        return
+
+    def get_statement(self):
+        combined_maps = input_settings.OPERATIONS['combined_maps']
+
+        for sys_maps in combined_maps:
+            for element in combined_maps[sys_maps]:
+                operation = Operation('great_equal')
+                operation.create()
+
+
+class FootprintMap(Statement):
+    OP = 'footprint_map'
+
+    def __init__(self):
+        self._input = input_settings.OPERATIONS
+
+        self._maps = []
+        self._unite = []
+        self._intersect = []
+
+        if BadRegions.OP in self._input:
+            bad_regions = Operation(BadRegions.OP)
+            self._maps.append(bad_regions)
+            self._unite.append(bad_regions.save_at())
+
+        if RadialMap.OP in self._input:
+            radial_map = Operation(RadialMap.OP)
+            self._maps.append(radial_map)
+            self._unite.append(radial_map.save_at())
+
+        if CombinedMaps.OP in self._input:
+            combined_maps = Operation(CombinedMaps.OP)
+            self._maps.extend(combined_maps.all_maps())
+            self._intersect.append(combined_maps.tables_to_intersect())
+
+    def get_statement(self):
+        return
+
+    def operation_name(self):
+        return FootprintMap.OP
+
+    def all_maps(self):
+        return self._maps
+
+
+class QueryBuilder(Statement):
+    def __init__(self):
+
+        self._footprint = Operation(FootprintMap.OP)
+        # self._catalog = Operation(Catalog.OP)
+
+    def all_operations(self):
+        return return self._footprint.all_maps()

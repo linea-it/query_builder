3ab59d1c523b255af6e0f71e4f21ea9400353cb0
lucasdpn <lucasdpnunes@gmail.com>
2017-02-13 10:57:23 -0200

    Only persistent tables are stored in the DB.

--- model/query_builder.py
+++ model/query_builder.py
@@ -14,7 +14,12 @@ class QueryBuilder():
     def __init__(self, tree, thread_pools=1):
         self.thread_pools = thread_pools
         self.operations = OrderedDict()
-        self.traverse_post_order(tree)
+        node, obj_op = self.traverse_post_order(tree)
+
+        # drop temporary tables. ROOT node.
+        if node.data['permanent_table'] is not True:
+            obj_op.delete()
+            del self.operations[node.data['name']]
 
     def traverse_post_order(self, node):
         sub_operations = {}
@@ -28,6 +33,13 @@ class QueryBuilder():
 
         obj_op = intermediate_table.IntermediateTable(node.data,
                                                       sub_operations)
+
+        # drop temporary tables.
+        for sub_node in node.sub_nodes:
+            if sub_node.data['permanent_table'] is not True:
+                sub_operations[sub_node.data['name']].delete()
+                del self.operations[sub_node.data['name']]
+
         self.operations[node.data['name']] = obj_op
         return node, obj_op
 

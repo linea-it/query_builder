3f3d6b3986d9169342f94dcd8becd27f693d14fb
lucasdpn <lucasdpnunes@gmail.com>
2017-02-15 16:36:52 -0200

    tests to all y1a1 subset

---
+++ test/test_queries_y1a1.py
@@ -0,0 +1,66 @@
+import unittest
+from mock import patch, Mock
+
+from utils.db import dal, DataAccessLayer
+
+from model.query_builder import QueryBuilder
+import model.tree as t
+from utils import util
+
+
+class test_operations(unittest.TestCase):
+    db = {
+        'dialect': 'postgresql',
+        'driver': 'psycopg2',
+        'username': 'gavo',
+        'password': 'gavo',
+        'host': 'localhost',
+        'port': '25432',
+    }
+    dal.db_init(DataAccessLayer.str_connection(db),
+                schema_output='tst_oracle_output')
+    base_path = "test/config_y1a1/"
+
+    @staticmethod
+    def get_operations(file_name):
+        path = test_operations.base_path + file_name
+        obj = util.load_json(path)
+        tree = t.tree_builder(obj)
+        return QueryBuilder(tree)
+
+    def setUp(self):
+        self.operations = None
+
+    def test_op_great_equal(self):
+        self.operations = test_operations.get_operations('great_equal.json')
+
+    def test_op_footprint_inner_left(self):
+        self.operations = test_operations.get_operations(
+                'footprint_inner_left.json')
+
+    def test_op_combined_maps(self):
+        self.operations = test_operations.get_operations(
+                'combined_maps.json')
+
+    def test_op_object_selection(self):
+        self.operations = test_operations.get_operations(
+                'object_selection.json')
+
+    # def test_op_sg_separation(self):
+    #     self.operations = test_operations.get_operations(
+    #             'sg_separation.json')
+
+    # def test_op_photoz(self):
+    #     self.operations = test_operations.get_operations(
+    #             'photoz.json')
+
+    # def test_op_galaxy_properties(self):
+    #     self.operations = test_operations.get_operations(
+    #             'galaxy_properties.json')
+
+    def tearDown(self):
+        self.operations.drop_all_tables()
+
+
+if __name__ == '__main__':
+    unittest.main()

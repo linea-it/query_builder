e835eaefe116e9a48a39743e9e73aa30dc6e4c45
lucasdpn <lucasdpnunes@gmail.com>
2017-02-17 17:45:27 -0200

    refactoring - 'switch' was changed to a dictionary.

--- model/operation_builder.py
+++ model/operation_builder.py
@@ -3,32 +3,26 @@ from model import operations
 
 """
     This is the Factory class to create new operations.
-    
+
     When a new operation is defined in models.opetations, we must add here the
 condition to create the apropriate class.
 """
 
 
 class OperationBuilder():
+    ops = {}
+    ops[operations.GreatEqual.OPERATION] = operations.GreatEqual()
+    ops[operations.CombinedMaps.OPERATION] = operations.CombinedMaps()
+    ops[operations.BadRegions.OPERATION] = operations.BadRegions()
+    ops[operations.Footprint.OPERATION] = operations.Footprint()
+    ops[operations.ObjectSelection.OPERATION] = operations.ObjectSelection()
+    ops[operations.PhotoZ.OPERATION] = operations.PhotoZ()
+    ops[operations.GalaxyProperties.OPERATION] = operations.GalaxyProperties()
+
     @staticmethod
     def create(operation_type):
-        if operation_type == operations.GreatEqual.OPERATION:
-            operation = operations.GreatEqual()
-        elif operation_type == operations.CombinedMaps.OPERATION:
-            operation = operations.CombinedMaps()
-        elif operation_type == operations.BadRegions.OPERATION:
-            operation = operations.BadRegions()
-        elif operation_type == operations.Footprint.OPERATION:
-            operation = operations.Footprint()
-        elif operation_type == operations.ObjectSelection.OPERATION:
-            operation = operations.ObjectSelection()
-        elif operation_type == operations.SgSeparation.OPERATION:
-            operation = operations.SgSeparation()
-        elif operation_type == operations.PhotoZ.OPERATION:
-            operation = operations.PhotoZ()
-        elif operation_type == operations.GalaxyProperties.OPERATION:
-            operation = operations.GalaxyProperties()
-        else:
+        try:
+            operation = OperationBuilder.ops[operation_type]
+            return operation
+        except:
             raise "This operation is not implemented."
-
-        return operation

e50271d2068926305c737a1ca0d1ad85cd5d1a01
lucasdpn <lucasdpnunes@gmail.com>
2017-04-11 14:14:49 -0300

    Using the lib networkx to deal with Graphs.

--- model/query_builder.py
+++ model/query_builder.py
@@ -1,8 +1,25 @@
 from collections import OrderedDict
-from multiprocessing.dummy import Pool as ThreadPool
+from random import randint
+import networkx as nx
+from threading import Thread, Lock
+import copy
 
 from model import intermediate_table
 
+
+def create_workflow_inverse_edges(graph):
+    inv_graph = nx.DiGraph()
+    for edge in list(graph.edges()):
+        inv_graph.add_edge(edge[1], edge[0])
+    return inv_graph
+
+
+def nodes_no_neighborhood(graph, nodes):
+    _nodes = []
+    for node in nodes:
+        if len(nx.edges(graph, node)) == 0:
+            _nodes.append(node)
+    return _nodes
 
 """
     Receiving a tree of operations, this class builds all the operations,
@@ -11,37 +28,53 @@ managing the construction in the right order allowing parallelism.
 
 
 class QueryBuilder():
-    def __init__(self, tree, thread_pools=1):
-        self.thread_pools = thread_pools
+    lock = Lock()
+
+    def __init__(self, op_description, workflow, thread_pools=1):
         self.operations = OrderedDict()
-        node, obj_op = self.traverse_post_order(tree)
 
-        # drop temporary tables. ROOT node.
-        if node.data['permanent_table'] is not True:
-            obj_op.delete()
-            del self.operations[node.data['name']]
+        self.op_description = op_description
+        self.thread_pools = thread_pools
 
-    def traverse_post_order(self, node):
-        sub_operations = {}
-        if node.sub_nodes:
-            pool = ThreadPool(self.thread_pools)
-            results = pool.map(self.traverse_post_order, node.sub_nodes)
-            pool.close()
-            pool.join()
-            for result in results:
-                sub_operations[result[0].data['name']] = result[1]
+        self.workflow = workflow
+        self.inv_workflow = create_workflow_inverse_edges(self.workflow)
+        self.updated_workflow = copy.deepcopy(self.workflow)
 
-        obj_op = intermediate_table.IntermediateTable(node.data,
-                                                      sub_operations)
+        self.create_tables(nodes_no_neighborhood(self.updated_workflow,
+                                                 self.updated_workflow.nodes()))
 
-        # drop temporary tables.
-        for sub_node in node.sub_nodes:
-            if sub_node.data['permanent_table'] is not True:
-                sub_operations[sub_node.data['name']].delete()
-                del self.operations[sub_node.data['name']]
+    def create_tables(self, nodes):
+        if nodes:
+            thread_list = []
+            for node in nodes:
+                t = Thread(target=self._run, args=(node,))
+                thread_list.append(t)
 
-        self.operations[node.data['name']] = obj_op
-        return node, obj_op
+            for thread in thread_list:
+                thread.start()
+
+            for thread in thread_list:
+                thread.join()
+        return
+
+    def _run(self, _id):
+
+        sub_ops_list = [i[1] for i in nx.edges(self.workflow, _id)]
+        sub_ops_dict = {k:self.operations[k] for k in sub_ops_list if k in self.operations}
+        self.op_description[_id]['name'] = _id
+        obj_op = intermediate_table.IntermediateTable(self.op_description[_id],
+                                                      sub_ops_dict)
+        self.operations[_id] = obj_op
+
+        for node in nx.edges(self.inv_workflow, _id):
+            self.updated_workflow.remove_edge(node[1], node[0])
+
+        with QueryBuilder.lock:
+            nodes = nodes_no_neighborhood(self.updated_workflow,
+                                          self.inv_workflow.neighbors(_id))
+
+        self.create_tables(nodes)
+        return
 
     def get(self):
         """

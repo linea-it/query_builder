30626fd7717014d6605ef5a01405a1fd813f9a3b
lucasdpn <lucasdpnunes@gmail.com>
2017-04-13 11:29:09 -0300

    Removed unused function

--- utils/util.py
+++ utils/util.py
@@ -6,30 +6,3 @@ import re
 def load_json(filename):
     with open(filename) as f:
         return json.load(f)
-
-
-def dot_file_to_dict(file_name):
-    COMMENT = "^\s*#"
-    NEW_LINE = "^\s*$"
-    ARC = "^\s*(\S+)\s*->\s*(\S+)\s*$"
-
-    tree = {}
-    with open(file_name) as f:
-        lines = f.read().splitlines()
-
-    for line in lines:
-        p = re.compile(COMMENT)
-        if p.match(line):
-            continue
-        p = re.compile(NEW_LINE)
-        if p.match(line):
-            continue
-        p = re.compile(ARC)
-        if p.match(line):
-            groups = p.match(line).groups()
-            if groups[0] not in tree:
-                tree[groups[0]] = []
-            tree[groups[0]].append(groups[1])
-            continue
-        raise "invalid format"
-    return tree
